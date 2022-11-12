dec0a34704e28443ab7e42e6627c399de60191bb
Luis Rojas <luisignacio1210@gmail.com>
2022-11-12 11:43:16 +0100

    adding a clock

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //const taskInput = document.querySelector('.input-task-input');
 //const clearAll = document.querySelector('.clear-btn');
+
+/*clock*/
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const date = new Date();
+document.getElementById("clock").innerHTML = date.toLocaleTimeString();
+}
