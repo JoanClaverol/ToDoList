185f0136b10904fb0e351556c475644d1c142d74
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2022-11-12 11:43:11 +0100

    working on reading tasks

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //const taskInput = document.querySelector('.input-task-input');
 //const clearAll = document.querySelector('.clear-btn');
+const taskTemplate = ``
+
+let taskList = [...document.getElementsByClassName('task')].forEach(
+    (e, i, a) => {
+        console.log(
+            e.getElementsByTagName('label')[0].getElementsByTagName('p')[0]);
+    }
+);
