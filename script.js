5dec37aa11f568ac0c138b20ce67b22c62967379
MirkoDuric <mirkodjuricczv@gmail.com>
2022-11-17 17:11:37 +0100

    Editing and deleting function finished

--- script.js
+++ script.js
@@ -1,41 +1,84 @@
-const editTaskButton = document.querySelectorAll(".edit-li");
 const checkBoxDone = document.querySelectorAll(".checkboxes");
 const taskLi = document.querySelectorAll(".task");
 const paragraphEdit = document.querySelectorAll(".paragraph-edit");
 const taskInput = document.querySelector('.input-task-input');
 
+
 // removing tasks form the list with delete button
 const removeTask = (el) => {
     el.target.parentNode.parentNode.parentNode.remove();
 };
-function addingEventListenerToDeleteButton(){
-    
-}
-
+function deletTaskEvent (){
+    const deleteTask = document.querySelectorAll(".delete-li");
+    deleteTask.forEach(deleteButton => {
+        deleteButton.addEventListener("click", (el) => {
+            removeTask(el);
+        })
+    } )
+};
 // editing tasks
-const editTask = (e) => {
+const editTask = (edit, el) => {
     console.log("edit task click working");
-    const parentElement = e.target.parentNode.parentNode.parentNode;
-     console.log(parentElement.querySelector("p"));
+    const parentElement = edit.target.parentNode.parentNode.parentNode;
+    const valueInput = parentElement.querySelector('p').innerHTML;
+    parentElement.innerHTML = `
+        <label>
+            <input type="checkbox">
+            <input id="newInputValue" type='text' value= ${valueInput}>
+        </label>
+        <div class="settings">
+            <i class="uil uil-ellipsis-h"></i>
+            <ul class="task-menu">
+                <li class="edit-li"><i class="uil uil-pen"></i>Edit</li>
+                <li class="delete-li"><i class="uil uil-trash"></i>Delete</li>
+            </ul>
+        </div>
+    `;
+    parentElement.addEventListener('keypress', (edit) => {
+        let newValue = parentElement.querySelector('#newInputValue').value;
+            if (edit.key === 'Enter') {
+                parentElement.innerHTML = `
+                <label>
+                    <input type="checkbox">
+                    <p>${newValue}</p>
+                </label>
+                <div class="settings">
+                    <i class="uil uil-ellipsis-h"></i>
+                    <ul class="task-menu">
+                        <li class="edit-li"><i class="uil uil-pen"></i>Edit</li>
+                        <li class="delete-li"><i class="uil uil-trash"></i>Delete</li>
+                    </ul>
+                </div>`;
+            }
+            deletTaskEvent ();
+            editTaskEvent ();
+        })
+
+    };
+function editTaskEvent() {
+    const editTaskButton = document.querySelectorAll(".edit-li");
+    editTaskButton.forEach(editButton => {
+        editButton.addEventListener("click", (edit) => {
+            editTask(edit);
+        })
+    });
 };
 
 
 taskInput.addEventListener('keypress', (e) => {
     if (e.key === "Enter") {
         if (taskInput.value) {
-            console.log('ok');
-            updateTasks();
-            taskInput.value = '';
-            const deleteTask = document.querySelectorAll(".delete-li");
-            for (let index = 0; index < deleteTask.length; index++) {
-                const element = deleteTask[index];
-                element.addEventListener("click", (el) => {
-                removeTask(el);
-        })
-    }
+                console.log('ok');
+                updateTasks();
+                taskInput.value = '';
+                // DELETE FUNCTION LISTENER
+                deletTaskEvent();
+                // EDIT FUNCTION LISTENER
+                editTaskEvent();
+            }
         }
     }
-});
+);
 
 const generateTask = (taskName) => {
     const task = document.createElement('li');
@@ -48,7 +91,7 @@ const generateTask = (taskName) => {
         <div class="settings">
             <i class="uil uil-ellipsis-h"></i>
             <ul class="task-menu">
-                <li class=""><i class="uil uil-pen"></i>Edit</li>
+                <li class="edit-li"><i class="uil uil-pen"></i>Edit</li>
                 <li class="delete-li"><i class="uil uil-trash"></i>Delete</li>
             </ul>
         </div>
