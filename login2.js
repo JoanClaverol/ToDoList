be9248310fee2e667d05ddf2e0de48a911140cc0
Luis Rojas <luisignacio1210@gmail.com>
2022-11-23 12:22:48 +0100

    adding login page

---
+++ login2.js
@@ -0,0 +1,30 @@
+
+document.querySelector('#button').addEventListener('click', (e) => { window.location.href = 'https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjdwti9kMT7AhVeQfEDHVhLB_4QPAg';
+});
+
+
+
+const button = document.getElementById('button')
+
+console.log(button);
+
+button.addEventListener('click', (e) => {
+   // e.preventDefault()
+    const userEmail = document.getElementById('email').value
+    const password = document.getElementById('pass').value
+    console.log(userEmail);
+    console.log(password);
+
+    const user = {
+        email: userEmail,
+        password: password,
+    }
+    console.log(user);
+});
+
+// function myFunction(){
+// getElementById("email").required; 
+// document.getElementById().innerHTML = x;
+// }
+
+
