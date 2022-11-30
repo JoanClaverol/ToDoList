c29ae5cc7d6331ce365e0ee2a8a195122f7268f8
MirkoDuric <mirkodjuricczv@gmail.com>
2022-11-30 18:13:41 +0100

    Sign up button creating object user in users array and log in functional.

--- login.js
+++ login.js
@@ -1,32 +1,52 @@
+const emailInput = document.querySelector(".email");
+const passwordInput = document.querySelector('.password');
+const logInButton = document.getElementById('submit-btn');
+const signUpButton = document.querySelector('.signUp');
 
-  const form = document.querySelector("form");
-  emailField = form.querySelector(".email");
-  emailInput = emailField.querySelector("input");
-  passwordField = form.querySelector(".password");
-  passwordInput = passwordField.querySelector('input');
+class Users {
+  constructor(email, password){
+    this.email = email,
+    this.password = password
+  }
+}
 
-  // form.onsubmit = (e) => {
-  //   e.preventDefault();
-  //   if(emailInput.value == "") {
-  //       alert("Please fill username");
-  //   }
-  //   if(passwordInput.value == "") {
-  //       alert("Please fill password");
-  // }
-  // }
 
-  //document.addEventListener("submit", (e) => {
-   // e.preventDefault();
 
-  //}
-// const logInButton = document.getElementById('submit-btn');
-// if(emailField.value == 'mirkodjuricczv@gmail.com' && passwordField.value == 'aaa'){
-//   logInButton.addEventListener('click', function(e) {
-//     document.location.assign('./index.html');
-//     e.preventDefault();
-//   })
-// }else{
-//   alert('Please insert correct password and email.')
-//   e.preventDefault()
-// }
+let users = [];
+
+logInButton.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('radi');
+  let emailValue = emailInput.value;
+  let passwordValue = passwordInput.value;
+  users.forEach(user => {
+    if(user.email === emailValue && user.password === passwordValue){
+      document.location.assign("./index.html");
+    }else{
+      alert('Please insert correct password and/or email.');
+    }
+  })
+})
+signUpButton.addEventListener('click', signUpFunction)
+function signUpFunction(){
+  let email = prompt('Please type in your e-mail adress');
+  let password = prompt('Please set up your password');
   
+  alert(email);
+  // if(!email.contains('@.com')){
+  //   alert('Enter correct form for email');
+  //   return alert(email);
+  // }
+  alert(password);
+  // if (password.length < 5) {
+  //   alert('Password has to be longer than 4 caracters')
+  //   return alert(password);
+  // }
+  let newUser = {
+    email,
+    password
+  }
+  users.push(newUser);
+  // localStorage.setItem(JSON.stringify({email}), JSON.stringify({password}));
+  // localStorage.clear();
+}
